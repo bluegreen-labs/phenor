45cb3d65ce1a72c6a9eebc78ccd0252d666ebd45
Koen Hufkens <khufkens@Pandora.local>
2017-05-03 20:51:28 +0200

    add model comparison plot

---
+++ R/LIN.r
@@ -0,0 +1,38 @@
+#' Linear model as defined in
+#' Basler et al. 2016 (Agr. For. Meteorlogy)
+#'
+#' @param data: a nested list of data
+#' @param par: a vector of parameter values, this is functions specific
+#' @param spring: a vector defining spring as a couple of DOY values
+#' default is March, April, May or DOY 60 - 151
+#' @keywords phenology, model, sequential
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' estimate = LIN(data = data, par = par)
+#'}
+
+LIN = function(par, data, spring = c(60,151)){
+
+  # exit the routine as some parameters are missing
+  if (length(par) != 2){
+    stop("model parameter(s) out of range (too many, too few)")
+  }
+
+  # extract the parameter values from the
+  # par argument for readability
+  a = par[1]
+  b = par[2]
+
+  # calculate location of "spring" values
+  spring_loc = phenocam_DB$doy %in% seq(spring[1],spring[2])
+
+  # calculate the mean temperature for this range
+  mean_spring_temp = apply(data$Ti,2,function(xt){
+    mean(xt[spring_loc])
+  })
+
+  # return fit values
+  return( a * mean_spring_temp + b )
+}
---
+++ R/plot_model_comparison.r
@@ -0,0 +1,72 @@
+#' Model comparison  plotting routine to faciliate model development
+#' and quick comparisons of the skill of various models.
+#'
+#' @param comparison: list returned by model_comparison()
+#' @param data: data driving model_comparison()
+#' @keywords phenology, model, data, comparison, plotting
+#' @export
+#' @examples
+#'
+#' \dontrun{
+#' plot_model_comparison()
+#' }
+
+plot_model_comparison = function(comparison = NULL,
+                                 data = NULL){
+
+  # trap lack of data
+  if (is.null(comparison) | is.null(data)){
+    stop("No comparison or reference data ")
+  }
+
+  # colours => ugly find solution
+  colours = as.data.frame(matrix(
+    c("NULL","black",
+    "LIN","black",
+    "TT","red",
+    "TTs","red",
+    "PTT","red",
+    "PTTs","red",
+    "M1","red",
+    "M1s","red",
+    "AT","blue",
+    "SQ","blue",
+    "SQb","blue",
+    "SM1","blue",
+    "SM1b","blue",
+    "PA","blue",
+    "PAb","blue",
+    "UN","blue",
+    "UM1","blue"),17,2, byrow = TRUE))
+  colnames(colours) = c("model","colour")
+
+  # calculate mean / sd RMSE of all model runs
+  # (different parameters - by different seeds)
+  rmse_stats = lapply(comparison,function(x){
+    rmse = apply(x$predicted_values,1,function(y){
+      sqrt(mean((y - data$transition_dates) ^ 2, na.rm = T))
+    })
+    return(rmse)
+    list("rmse"=mean(rmse,na.rm=TRUE),"sd"=sd(rmse,na.rm=TRUE))
+    })
+
+  labels = names(rmse_stats)
+  col_sel = as.character(colours[which(colours$model %in% labels),2])
+  rmse_stats = do.call("cbind",rmse_stats)
+
+  # calculate RMSE null model (single value)
+  rmse_null = sqrt(mean((data$transition_dates - null(data)) ^ 2, na.rm = T))
+
+  # create boxplot with stats
+  boxplot(rmse_stats,
+          ylim = c(0, 16),
+          ylab = "RMSE (days)",
+          whiskcol = col_sel,
+          staplecol = col_sel,
+          boxcol = col_sel,
+          medcol = col_sel,
+          outline = FALSE)
+
+  # set a horizontal marker for the baseline NULL model
+  abline(h = rmse_null)
+}
