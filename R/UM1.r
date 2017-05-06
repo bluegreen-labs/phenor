9bdc4a161351dd085303fc0746f2c90b9a71dcb0
Koen Hufkens <khufkens@Pandora.local>
2017-05-06 21:16:16 +0200

    minor tweaks to various functions

--- R/UM1.r
+++ R/UM1.r
@@ -37,8 +37,8 @@ UM1  = function(par, data){
 
   # chilling
   Rc = matrix(0,nrow(data$Ti),ncol(data$Ti)) # allocate empty matrix
-  Rc[data$Ti < T_opt & data$Ti >= T_min] = (Rc[data$Ti < T_opt & data$Ti >= T_min] - T_min)/(T_opt - T_min)
-  Rc[data$Ti < T_max & data$Ti >= T_opt] = (Rc[data$Ti < T_max & data$Ti >= T_opt] - T_opt)/(T_max - T_opt)
+  Rc[data$Ti < T_opt & data$Ti >= T_min] = (data$Ti[data$Ti < T_opt & data$Ti >= T_min] - T_min)/(T_opt - T_min)
+  Rc[data$Ti < T_max & data$Ti >= T_opt] = (data$Ti[data$Ti < T_max & data$Ti >= T_opt] - T_opt)/(T_max - T_opt)
   Rc[1:t0_chill,] = 0
   Rc[t0:nrow(Rc),] = 0
   Sc = apply(Rc,2, cumsum)
