17fe16112d2b094c29496732a6b2e442bc295544
Koen Hufkens <khufkens@Pandora-2.local>
2017-09-21 16:52:51 +0200

    split out downloading routine

--- R/format_berkeley_earth.r
+++ R/format_berkeley_earth.r
@@ -22,50 +22,27 @@ format_berkeley_earth = function(path = "~",
                                  bounding_box = c(-126, -66, 23, 54),
                                  internal = TRUE){
 
-  # set server
-  server = "http://berkeleyearth.lbl.gov/auto/Global/Gridded"
+  # download all data if it doesn't exist
+  download_berkeley_earth(path = path,
+                          year = year)
 
   # set the decadal splits
   split = seq(1880,as.numeric(format(Sys.Date(),"%Y")),10)
 
+  # create download string
   # download 2 files if on split decade
   if (year %in% split){
-    # create download string
-    filenames = c(sprintf("Complete_TAVG_Daily_LatLong1_%s.nc",
-                          split[max(which(split <= year),na.rm=TRUE) - 1]),
-                  sprintf("Complete_TAVG_Daily_LatLong1_%s.nc",
-                          split[max(which(split <= year),na.rm=TRUE)]))
-
+    tavg_files = c(sprintf("Complete_TAVG_Daily_LatLong1_%s.nc",
+                           split[max(which(split <= year),na.rm=TRUE) - 1]),
+                   sprintf("Complete_TAVG_Daily_LatLong1_%s.nc",
+                           split[max(which(split <= year),na.rm=TRUE)]))
   } else {
-    # create download string
-    filenames = sprintf("Complete_TAVG_Daily_LatLong1_%s.nc",
-                        split[max(which(split <= year),na.rm=TRUE)])
+    filenames = c(sprintf("Complete_TAVG_Daily_LatLong1_%s.nc",
+                          split[max(which(split <= year),na.rm=TRUE)]))
   }
 
-  # download missing data
-  for (i in filenames){
-
-    # set download / filename strings
-    file_location = sprintf("%s/%s",path,i)
-    http_location = sprintf("%s/%s", server, i)
-
-    if(!file.exists(file_location)){
-      # try to download the data
-      error = try(curl::curl_download(http_location,
-                                      file_location,
-                                      mode="w",
-                                      quiet=TRUE),silent=TRUE)
-      if (inherits(error, "try-error")){
-        # remove file header
-        file.remove(file_location)
-        stop("failed to download the requested data, check your connection")
-      }
-    } else {
-      cat("local file exists, skipping download \n")
-    }
-  }
-
-  # grab raster data from netcdf files
+  # grab raster data from netcdf files, with the climatology
+  # the long term mean and delta the differences
   climatology = raster::brick(sprintf("%s/%s",path,filenames[1]),
                               varname = "climatology")
   delta = do.call("stack",
@@ -78,6 +55,7 @@ format_berkeley_earth = function(path = "~",
     nc = ncdf4::nc_open(sprintf("%s/%s",path,x))
     ncdf4::ncvar_get(nc,"year")
   }))
+
   yday = do.call("c",lapply(filenames,FUN = function(x){
     nc = ncdf4::nc_open(sprintf("%s/%s",path,x))
     ncdf4::ncvar_get(nc,"day_of_year")
@@ -90,17 +68,17 @@ format_berkeley_earth = function(path = "~",
   # check if all layers are available in the dataset
   # needs a fix for crossing boundaries between decadal subsets !!
   if (length(layers) < 365){
-    stop("The selected Berkeley Earth dataset does not cover your data range!")
+    stop("The selected Berkeley Earth data doesn't cover your time frame!")
   }
 
   # subset raster data, do this before cropping to make things faster
   delta = raster::subset(delta, layers)
 
-  # crop data if necessary
+  # crop data if necessary, no checks yet
   if (!is.null(bounding_box)){
     if(length(bounding_box)==4){
-      climatology = raster::crop(climatology,extent(bounding_box))
-      delta = raster::crop(delta,extent(bounding_box))
+      climatology = raster::crop(climatology, extent(bounding_box))
+      delta = raster::crop(delta, extent(bounding_box))
     } else {
       stop("not enough coordinate points to properly crop data!")
     }
@@ -115,12 +93,12 @@ format_berkeley_earth = function(path = "~",
 
   # calculate absolute temperatures not differences
   # with the mean
-  temperature = raster::stackApply(raster::stack(delta,climatology),
-                           indices = c(doy,1:length(layers)),
-                           fun = sum )
+  temperature = raster::stackApply(raster::stack(delta, climatology),
+                                   indices = c(doy,1:length(layers)),
+                                   fun = sum )
   temperature[temperature == 0] = NA
 
-  # convert temperature data to matrix use the raster as.matrix() function!!
+  # convert temperature data to matrix
   Ti = t(raster::as.matrix(temperature))
 
   # extract georeferencing info to be passed along
@@ -132,7 +110,7 @@ format_berkeley_earth = function(path = "~",
 
   # grab coordinates
   location = sp::SpatialPoints(coordinates(temperature),
-                           proj4string = CRS(proj))
+                               proj4string = CRS(proj))
   location = t(sp::spTransform(location, CRS("+init=epsg:4326"))@coords[,2:1])
 
   # create doy vector
@@ -156,8 +134,11 @@ format_berkeley_earth = function(path = "~",
               "doy" = doy,
               "transition_dates" = NULL,
               "Ti" = Ti,
+              "Tmini" = NULL,
+              "Tmaxi" = NULL,
               "Li" = Li,
               "Pi" = NULL,
+              "VPDi" = NULL,
               "georeferencing" = list("extent" = ext,
                                       "projection" = proj,
                                       "size" = size)
