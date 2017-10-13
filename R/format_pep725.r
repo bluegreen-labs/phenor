dc0da671efa36201e06b3293c069fcf23422cfd8
Koen Hufkens <koen.hufkens@gmail.com>
2017-10-13 20:58:09 +0200

    help file corrections, include new climate variables, correct class

--- R/format_pep725.r
+++ R/format_pep725.r
@@ -1,12 +1,13 @@
 #' Preprocessing of PEP725 data into a format which can be ingested
-#' by the optimization routines, the original nested list is flattened
-#' for speed. The routine requires E-OBS data, which can be downloaded from
-#' the E-OBS website (http://www.ecad.eu/download/ensembles/ensembles.php).
+#' by the optimization routines.
 #'
 #' Some pre-processing steps are required as downloading the PEP725 data is a
-#' mess (this database needs an API). So, for the species of interest
-#' download the separate zipped files. Unzip all files and put the respective
-#' scientific data in one folder.
+#' mess. So, for the species of interest download the separate zipped files.
+#' Unzip all files and put the respective scientific data in one folder.
+#'
+#' The routine requires E-OBS climate, which can be downloaded from
+#' the E-OBS website and should be put unzipped in a single folder.
+#' (http://www.ecad.eu/download/ensembles/ensembles.php).
 #'
 #' @param pep_path path to the PEP725 data (species files only)
 #' @param eobs_path path to regular grid E-OBS data.
@@ -27,7 +28,8 @@
 #' # through phenocamr in your home directory
 #' # change the path to match your setup
 #' \dontrun{
-#' phenocam_data = format_pep725()
+#' phenocam_data = format_pep725(pep_path = "~/pep_data/",
+#'                               eobs_path = "~/eobs_data/")
 #'}
 
 format_pep725 = function(pep_path = "~",
@@ -54,17 +56,25 @@ format_pep725 = function(pep_path = "~",
 
     # subset raster data (only once, then replicate data for speed)
     # [subsetting the raster data is a bottleneck]
-    temperature = raster::extract(eobs_data[[1]], points)
+    tmean = raster::extract(eobs_data[[1]], points)
+    tmin = raster::extract(eobs_data[[4]], points)
+    tmax = raster::extract(eobs_data[[5]], points)
 
     # close to water bodies raster cells can be empty and return
     # NA, in this case set the output to NULL and remove these
     # values later on
-    if (all(is.na(temperature))){
+    if (all(is.na(tmean))){
       return(NULL)
     } else {
       precipitation = raster::extract(eobs_data[[2]], points)
-      temperature = matrix(rep(temperature, nrow(pep_subset)),
-                           length(temperature),
+      tmean = matrix(rep(tmean, nrow(pep_subset)),
+                           length(tmean),
+                           nrow(pep_subset))
+      tmin = matrix(rep(tmin, nrow(pep_subset)),
+                           length(tmean),
+                           nrow(pep_subset))
+      tmax = matrix(rep(tmax, nrow(pep_subset)),
+                           length(tmean),
                            nrow(pep_subset))
       precipitation = matrix(rep(precipitation, nrow(pep_subset)),
                              length(precipitation),
@@ -72,7 +82,7 @@ format_pep725 = function(pep_path = "~",
     }
 
     # calculate long term mean temperature
-    ltm = as.vector(unlist(by(temperature[which(years >= 1980),1],
+    ltm = as.vector(unlist(by(tmean[which(years >= 1980),1],
              INDICES = yday[which(years >= 1980)],
              mean,
              na.rm = TRUE)))[1:365]
@@ -81,12 +91,26 @@ format_pep725 = function(pep_path = "~",
     lapse_rate = as.vector(
       (raster::extract(eobs_data[[3]],
                        points[1]) - pep_subset$alt[1]) * 0.005)
-    temperature = rbind(temperature + lapse_rate, pep_subset$year)
+    tmean = rbind(tmean + lapse_rate, pep_subset$year)
+    tmin = rbind(tmin + lapse_rate, pep_subset$year)
+    tmax = rbind(tmax + lapse_rate, pep_subset$year)
     ltm = ltm + lapse_rate
 
     # now for all years create subsets for temperature
-    # and precipitation
-    Ti = apply(temperature, 2, function(x){
+    # and precipitation (wrap this in a function and a do call)
+    Ti = apply(tmean, 2, function(x){
+      layers = which((years == (x[length(x)] - 1) & yday >= offset) |
+                       (years == x[length(x)] & yday < offset))[1:365]
+      return(x[layers])
+    })
+
+    Tmini = apply(tmin, 2, function(x){
+      layers = which((years == (x[length(x)] - 1) & yday >= offset) |
+                       (years == x[length(x)] & yday < offset))[1:365]
+      return(x[layers])
+    })
+
+    Tmaxi = apply(tmax, 2, function(x){
       layers = which((years == (x[length(x)] - 1) & yday >= offset) |
                        (years == x[length(x)] & yday < offset))[1:365]
       return(x[layers])
@@ -98,10 +122,15 @@ format_pep725 = function(pep_path = "~",
       return(x[layers])
     })
 
-    # create a doy vector
-    doy = c(offset:365,1:(offset - 1))
+    # shift data when offset is < 365
+    if (offset < 365){
+      doy_neg = c((offset - 366):-1,1:(offset - 1))
+      doy = c(offset:365,1:(offset - 1))
+    } else {
+      doy = doy_neg = 1:365
+    }
 
-    # calculate daylength
+    # calculate daylength using the doy
     l = ncol(Ti)
     Li = daylength(doy = doy, latitude = pep_subset$lat[1])
     Li = matrix(rep(Li,l),length(Li),l)
@@ -110,26 +139,23 @@ format_pep725 = function(pep_path = "~",
     # but with concatted data
     data = list("site" = site,
                 "location" = c(pep_subset$lat[1], pep_subset$lon[1]),
-                "doy" = doy,
+                "doy" = doy_neg,
                 "ltm" = ltm,
                 "transition_dates" = pep_subset$day,
                 "year" = pep_subset$year,
                 "Ti" = Ti,
-                "Tmini" = NULL,
-                "Tmaxi" = NULL,
+                "Tmini" = Tmini,
+                "Tmaxi" = Tmaxi,
                 "Li" = Li,
                 "Pi" = Pi,
                 "VPDi" = NULL
     )
 
-    # assign a class for post-processing
-    class(data) = "phenor_time_series_data"
-
     # return the list
     return(data)
   }
 
-  cat("* Merging PEP725 data files in: \n")
+  cat("* Merging and cleaning PEP725 data files in: \n")
   cat(sprintf("  %s\n", pep_path))
   # concat the raw PEP725 data
   pep_data = merge_pep725(path = pep_path)
@@ -152,9 +178,10 @@ format_pep725 = function(pep_path = "~",
   cat(sprintf(" |_ selecting phenophase: %s \n", bbch))
   pep_data = pep_data[which(pep_data$bbch == bbch),]
 
-  cat(sprintf(" |_ excluding sites with less than: %s observations \n", count))
+  cat(sprintf(" |_ excluding sites with: < %s site years of observations \n", count))
   # count number of observations for each
-  # unique pep725 site
+  # unique pep725 site, this does not screen
+  # for them being continuous or not !
   sites = unique(pep_data$pep_id)
   counts = unlist(lapply(sites,function(x){
       length(which(pep_data$pep_id == x))
@@ -179,10 +206,10 @@ format_pep725 = function(pep_path = "~",
   # loading E-OBS data for subsetting
   cat(sprintf("* Extracting E-OBS climatology for %s sites\n ", length(sites)))
 
-  # download or read data
-  eobs_data = lapply(c("tg","rr","elev"),function(x){
+  # read E-OBS data
+  eobs_data = lapply(c("tg","rr","elev","tn","tx"),function(x){
     # filename
-    filename = sprintf("%s_%sdeg_reg_v15.0.nc",
+    filename = sprintf("%s_%sdeg_reg_v16.0.nc",
                        x,
                        resolution)
 
@@ -212,6 +239,9 @@ format_pep725 = function(pep_path = "~",
   # add proper list names (this should be the same name as the
   # site name added by the format_data() helper function)
   names(validation_data) = sites
+
+  # assign a class for post-processing
+  class(validation_data) = "phenor_time_series_data"
 
   # screen for NULL values (climate grid cells with NA - near water bodies)
   validation_data = validation_data[!unlist(lapply(validation_data,is.null))]
