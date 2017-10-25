d73def1074c795f3a355ca20dad9d5762ad3ec05
Koen Hufkens <koen.hufkens@gmail.com>
2017-10-25 15:11:41 +0200

    new documentation

--- R/data.r
+++ R/data.r
@@ -15,6 +15,23 @@
 #' }
 "phenocam_DB"
 
+#' phenocam_DB_autumn
+#'
+#' Data structure containing leaf senescence dates for all deciduous broadleaf
+#' (DB) sites included in the first PhenoCam data release. These data
+#' are described in detail in Richardson et al. (2017)
+#'
+#' @format A structured list with 6 variables:
+#' \describe{
+#'   \item{site}{PhenoCam site name}
+#'   \item{location}{geographic location}
+#'   \item{doy}{DOY (1 - 365 as a vector)}
+#'   \item{transition_dates}{budburst dates, as per 25 percent of the Gcc amplitude }
+#'   \item{Ti}{mean daily temperature, matrix 365 rows x # site years columns}
+#'   \item{Li}{daylength, matrix 365 rows x # site years columns}
+#' }
+"phenocam_DB_autumn"
+
 #' phenocam_EN
 #'
 #' Data structure containing budburst dates for all evergreen needleleaf
