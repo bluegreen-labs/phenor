db83a8c276270688f467704bb828f3f25f28a6a0
khufkens <koen.hufkens@gmail.com>
2018-02-07 14:23:22 +0100

    bugfixes in the npn_check_species() code + docs

--- R/check_npn_species.r
+++ R/check_npn_species.r
@@ -1,6 +1,9 @@
 #' Checks if USA-NPN species exists
 #'
-#' @param species An USA-NPN species (character or number)
+#' @param species An USA-NPN species (character or number).
+#' Will search in both Genus species and common name fields and will match
+#' any term within those fields. The search relies on regular expressions so
+#' this can be used to be more specific.
 #' @param list List all species numbers and names as verbose output
 #' @return a validated list of species numbers, if not a warning is thrown
 #' and any depended routines halted.
@@ -17,24 +20,24 @@ check_npn_species = function(species = NULL,
                              list = TRUE){
   # download species information
   species_list = jsonlite::fromJSON("http://www.usanpn.org/npn_portal/species/getSpecies.json")
+  species_list = subset(species_list, select = -species_type)
 
-  # drop last column
-  species_list = subset(species_list, select = -species)
-
-  # check if the phenophase exists, if provided
+  # check if the genus exists, if provided
   if(!is.null(species)){
+
     if(any(species %in% species_list$species_id |
            grepl(tolower(species), tolower(paste(species_list$genus, species_list$species)))
     )){
 
       # print info if requested
       if(list){
-        print(species_list[grepl(tolower(species),
+        return(species_list[grepl(tolower(species),
                                  tolower(paste(species_list$genus,
                                                species_list$species))) |
                              species_list$species_id %in% species,])
+      } else {
+        return(TRUE)
       }
-      return(TRUE)
     } else {
       return(FALSE)
     }
@@ -42,7 +45,7 @@ check_npn_species = function(species = NULL,
 
     # if nothing is requested for filtering, just return the full list
     # and a warning
-    print(species_list)
+    return(species_list)
     warning("No phenophase provided for further validation.")
   }
 }
