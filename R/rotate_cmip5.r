02af147f6c66e0589b5a6ab257b6ed0958c46f71
Koen Hufkens <koen.hufkens@gmail.com>
2018-09-28 11:30:01 -0400

    :: notattion in rotate function

--- R/rotate_cmip5.r
+++ R/rotate_cmip5.r
@@ -4,7 +4,7 @@
 #' raster rotate() command, as data is cropped
 #' before any translations (reducing memory load)
 #'
-#' @param r raster layer, stack or brick
+#' @param r CMIP5 raster layer, stack or brick
 #' @param extent vector with coordinates defining the region of interest defined
 #' as xmin, xmax, ymin, ymax in lat/lon (default = c(-74,-65, 40, 48))
 #' @return Returns raster data in the same format (layer, stack, brick) as
@@ -23,18 +23,18 @@ rotate_cmip5 <- function(r = NULL,
   if (extent[1] < 0){
     extent2[1] = 360 + extent[1]
   } else {
-    m = crop(r, extent)
+    m = raster::crop(r, extent)
   }
 
   # check maximum longitude value
   if (extent[2] < 0){
     extent2[2] = 360 + extent[2]
-    m = shift(crop(r, extent2), x = -360)
+    m = raster::shift(raster::crop(r, extent2), x = -360)
   } else {
     extent2[2] = 360
     extent[1] = 0
-    m = mosaic(shift(crop(r, extent2), x = -360),
-               crop(r, extent),fun=max)
+    m = raster::mosaic(raster::shift(raster::crop(r, extent2), x = -360),
+               raster::crop(r, extent),fun=max)
   }
 
   # return mosaic
