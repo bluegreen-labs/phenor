de785d38c18052617ad81b04f74b24715208ad4b
Koen Hufkens <koen.hufkens@gmail.com>
2018-10-07 16:51:07 -0700

    corection to rotation routine

--- R/rotate_cmip5.r
+++ R/rotate_cmip5.r
@@ -13,28 +13,30 @@
 #' @export
 
 rotate_cmip5 <- function(r = NULL,
-                           extent = c(-74,-65, 40, 48)){
+                           extent = c(-74, -65, 40, 48)){
 
   # duplicate the extent values
   # to swap out the longitude ones
   extent2 = extent
 
   # check minimum longtiude value
+  # correct for the offset
   if (extent[1] < 0){
     extent2[1] = 360 + extent[1]
-  } else {
-    m = raster::crop(r, extent)
   }
 
   # check maximum longitude value
+  # check for offset
   if (extent[2] < 0){
     extent2[2] = 360 + extent[2]
     m = raster::shift(raster::crop(r, extent2), x = -360)
-  } else {
+  } else if (extent[1] < 0 & extent[2] > 0) {
     extent2[2] = 360
     extent[1] = 0
     m = raster::mosaic(raster::shift(raster::crop(r, extent2), x = -360),
                raster::crop(r, extent),fun=max)
+  } else {
+    m = raster::crop(r, extent)
   }
 
   # return mosaic
