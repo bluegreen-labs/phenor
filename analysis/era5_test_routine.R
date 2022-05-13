3bebb9cb9b68e1b078db29fc1cbd9d9461378ffe
khufkens <koen.hufkens@gmail.com>
2022-05-13 14:44:40 +0200

    generate plots

--- analysis/era5_test_routine.R
+++ analysis/era5_test_routine.R
@@ -1,34 +1,36 @@
-#library(phenor)
-# library(ecmwfr)
-# options(keyring_backend="file")
-# source("R/pr_dl_era5.R")
-#
-# data <- pr_dl_era5(
-#   path = "~/Desktop",
-#   user = "2088",
-#   extent = c(
-#     45,
-#     -4,
-#     42,
-#     1.2
-#     )
-#   )
-#
-# r <- terra::rast("~/Desktop/era5.nc")
-# terra::plot(r$t2m_1)
-# maps::map("world", add = TRUE)
 library(phenor)
+library(ecmwfr)
+options(keyring_backend="file")
+
+source("R/pr_dl_era5.R")
+
+data <- pr_dl_era5(
+  path = "~/Desktop",
+  user = "2088",
+  product = "era5",
+  extent = c(
+    50.73149477111302,
+    -7.08887567473501,
+    40.365567456020266,
+    12.748594284373073
+    )
+  )
+
+data <- pr_dl_era5(
+  path = "~/Desktop",
+  user = "2088",
+  product = "land",
+  extent = c(
+    48.345183009475015, 4.782986565238425,
+    45.64153500799514, 11.44515031394129
+  )
+)
+
 source("R/pr_fm_era5.R")
 
 data <- pr_fm_era5(
   path = "~/Desktop/",
-  year = 2019,
-  extent = c(
-      -4,
-      1.1,
-      41,
-      45
-      )
+  year = 2019
 )
 
 
@@ -51,3 +53,4 @@ output <- pr_predict(
   )
 
 raster::plot(output)
+maps::map("world", add = TRUE)
